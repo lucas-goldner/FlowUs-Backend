1808adc4bbdbf6c4ea61b7f677540c0e99e8ea97
lucas-goldner <lucas.goldner@googlemail.com>
2022-05-23 00:01:36 +0200

    doc: Document most important parts of verify function

--- functions/src/functions/verify.ts
+++ functions/src/functions/verify.ts
@@ -8,10 +8,12 @@ import * as fs from "fs";
 import i18n from "../helpers/locales";
 
 const verifyHandler = region("europe-west1").https.onCall(async (data, context) => {
+  // Handle failing appcheck
   if (context.app == undefined) {
     throw new https.HttpsError("failed-precondition", "The function must be called from an App Check verified app.");
   }
 
+  // Handle missing data required for this function
   if (data == undefined || data.userName == undefined || data.locale == undefined || data.email == undefined) {
     const message: Message = {
       message: "Not all data was provided",
@@ -22,6 +24,9 @@ const verifyHandler = region("europe-west1").https.onCall(async (data, context) 
   }
 
   logger.log("All data for verification data is defined");
+
+  // Setup important variables
+  i18n.setLocale(data.locale);
   const uid = context?.auth?.uid;
   let result = "";
   const characters = "0123456789";
@@ -31,12 +36,14 @@ const verifyHandler = region("europe-west1").https.onCall(async (data, context) 
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
+  // Create document in firestore
   await db.collection(VERFIY_CODES_PATH).doc().set({
     user: uid,
     code: result,
   });
   logger.log("Firestore document was created for " + uid);
 
+  // Setup mail instance
   const transporter = nodemailer.createTransport({
     host: String(process.env.MAIL_HOST),
     port: Number(process.env.MAIL_PORT),
@@ -47,9 +54,9 @@ const verifyHandler = region("europe-west1").https.onCall(async (data, context) 
     },
   });
 
-  i18n.setLocale(data.locale);
-
   let mailContent = fs.readFileSync(__dirname + "/../assets/verifyEmail.html", "utf-8");
+
+  // Go over mail and replace portions with language specific parts
   mailContent = mailContent.replace(/WELCOME/g, i18n.__("Welcome"));
   mailContent = mailContent.replace(/STEPS/g, i18n.__("List"));
   mailContent = mailContent.replace(/VERIFY/g, i18n.__("Verify"));
@@ -62,6 +69,7 @@ const verifyHandler = region("europe-west1").https.onCall(async (data, context) 
   mailContent = mailContent.replace(/SINCERELY/g, i18n.__("Sincerely"));
   mailContent = mailContent.replace(/CTO/g, i18n.__("CTO"));
 
+  // Prepare mail
   const mailOptions = {
     from: '"Lucas 🍋" <lucas@flowus.io>',
     to: data.email,
@@ -69,6 +77,7 @@ const verifyHandler = region("europe-west1").https.onCall(async (data, context) 
     html: mailContent,
   };
 
+  // Send mail
   await transporter.sendMail(mailOptions).catch((e: Error) => {
     logger.error("Email creation has failed with: " + e.message);
     const message: Message = {
@@ -80,6 +89,7 @@ const verifyHandler = region("europe-west1").https.onCall(async (data, context) 
   });
 
   logger.log("Email has been created");
+
   const message: Message = {
     message: "Code was created and email is sent!",
     code: 200,
